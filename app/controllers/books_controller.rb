c72ce4cb55473fd96691b9aabfaec4267a8fb48f
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-08-13 11:03:27 +0000

    [Update] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,15 @@
 class BooksController < ApplicationController
   def index
     @lists = List.all
-  end
-  def new
-    @list = list.new
-  end
-  def create
     @list = List.new
-    @list.save
-    redirect_to 
+  end
+  # def new
+  #   @list = list.new
+  # end
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to "/"
   end
   def show
     # list = List.find(params[:id])
@@ -22,6 +23,9 @@ class BooksController < ApplicationController
   def destroy
     
   end
-  
-  
+   private
+  def list_params 
+    params.require(:list).permit(:title,:body)
+  end 
+
 end
